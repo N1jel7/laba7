f9835853be68eea9252946067c7c1a6949f50481
Vanya Prokopovich <vanojefferson@gmail.com>
2023-10-30 20:24:41 +0300

    Added mark checker

--- laba7_variant11/laba7_variant11.cpp
+++ laba7_variant11/laba7_variant11.cpp
@@ -15,11 +15,9 @@ struct Exam { // создание структуры
     string town;
     string street;
     string house;
-    int mark1;
-    int mark2;
-    int mark3;
+    int marks[3];
     int averageMark;
-} mark[3], student[15]; // массив из кол-ва абитуриентов
+} students[25]; // массив из кол-ва абитуриентов
 
 
 int main()
@@ -29,25 +27,35 @@ int main()
     cin >> numOfStudents; // считывание кол-ва абитуриентов
     for (int i = 0; i < numOfStudents; i++) { // цикл для заполнения всех данных абитуриентов
         cout << "Введите ФИО абитуриента: " << endl; // заполнение ФИО
-        cin >> student[i].surname;
-        cin >> student[i].name;
-        cin >> student[i].lastName;
+        cin >> students[i].surname;
+        cin >> students[i].name;
+        cin >> students[i].lastName;
+
         cout << "Введите адресс абитуриента (Город-улица-№дома/квартиры): " << endl; // заполнение адресса
-        cin >> student[i].town;
-        cin >> student[i].street;
-        cin >> student[i].house;
-        cout << "Введите 1-ю оценку абитуриента: " << endl; // заполнение оценок
-        cin >> student[i].mark1;
-        cout << "Введите 2-ю оценку абитуриента: " << endl;
-        cin >> student[i].mark2;
-        cout << "Введите 3-ю оценку абитуриента: " << endl;
-        cin >> student[i].mark3;
+        cin >> students[i].town;
+        cin >> students[i].street;
+        cin >> students[i].house;
+        bool isCorrect = false;
+        while (!isCorrect) {
+            if (students[i].marks[0, 1, 2] >= 1 && students[i].marks[0, 1, 2] <= 100) {
+                isCorrect = true;
+            }
+            else {
+                cout << "Введите 1-ю оценку абитуриента: " << endl; // заполнение оценок
+                cin >> students[i].marks[0];
+                cout << "Введите 2-ю оценку абитуриента: " << endl;
+                cin >> students[i].marks[1];
+                cout << "Введите 3-ю оценку абитуриента: " << endl;
+                cin >> students[i].marks[2];
+            }
+        }
+        
     }
 
     for (int i = 0; i < numOfStudents; i++) { // цикл для расчёта среднего балла
-        student[i].averageMark = (student[i].mark1 + student[i].mark2 + student[i].mark3) / 3; // средний балл
-        if (student[i].averageMark >= 45 && isInMinsk(student[i].town)) { // условие для 45 баллов и выше
-            cout << "Средний бал " << student[i].surname << " равен: " << student[i].averageMark; // вывод с/б
+        students[i].averageMark = (students[i].marks[0] + students[i].marks[1] + students[i].marks[2]) / 3; // средний балл
+        if (students[i].averageMark >= 45 && isInMinsk(students[i].town)) { // условие для 45 баллов и выше
+            cout << "Средний бал " << students[i].surname << " равен: " << students[i].averageMark; // вывод с/б
             cout << ", также он живёт в Минске. ";
         }
 
